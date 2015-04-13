d4f1fb2658a3661421c0f9fdeae3107b08fe5b25
eteurnberg <teurnberg@hotmail.com>
2015-04-13 18:28:35 +0100

    All functions in Board.hs defined.
    Adds HUnit as a dependency for future implementation of tests.
    
    A couple of possible changes are mentioned in the comments.

--- src/Board.hs
+++ src/Board.hs
@@ -1,13 +1,21 @@
 module Board where
 
+import Data.Maybe
+
 data Col = Black | White
-  deriving Show
+    deriving (Show, Eq)
+
+data Direction = NorthEast | North | NorthWest | West | SouthWest | South | SouthEast | East
+    deriving Show
+
+--AllDirections = [NorthEast, North, NorthWest, West, SouthWest, South, SouthEast, East]
 
 other :: Col -> Col
 other Black = White
 other White = Black
 
 type Position = (Int, Int)
+
 
 -- A Board is a record containing the board size (a board is a square grid,
 -- n * n), the number of pieces in a row required to win, and a list 
@@ -26,13 +34,12 @@ data Board = Board { size :: Int,
 -- Default board is 6x6, target is 3 in a row, no initial pieces
 initBoard = Board 6 3 []
 
--- Overall state is the board and whose turn it is, plus any further
--- information about the world (this may later include, for example, player
--- names, timers, information about rule variants, etc)
---
 -- Feel free to extend this, and 'Board' above with anything you think
 -- will be useful (information for the AI, for example, such as where the
 -- most recent moves were).
+
+-- List of moves probably not needed as the list of pieces (in the board) is an ordered 
+-- history of moves anyway. 
 data World = World { board :: Board,
                      turn :: Col }
 
@@ -41,50 +48,112 @@ initWorld = World initBoard Black
 -- Play a move on the board; return 'Nothing' if the move is invalid
 -- (e.g. outside the range of the board, or there is a piece already there)
 makeMove :: Board -> Col -> Position -> Maybe Board
-makeMove board color pos = 
-    | (posWithinBounds pos board) && (cellAtPosIsEmpty pos board)
-        = board (size board) (target board) ((pieces board):(pos, color))
+makeMove board color pos
+    | isCellValidMove pos board = Just (Board (size board) (target board) ((pos, color):(pieces board)))
     | otherwise = Nothing
 
+-- Checks if a position is valid to move to.
+-- If cell is within bounds and empty.
+isCellValidMove :: Position -> Board -> Bool
+isCellValidMove pos board
+    | (posWithinBounds pos board) && (cellAtPosIsEmpty pos board) = True
+    | otherwise                                                   = False
 
 -- Checks if position is within bounds of board.
 posWithinBounds :: Position -> Board -> Bool
-posWithinBounds pos board = 
-    if(fst(pos) < 0)
-        False
-    if (snd(pos) < 0)
-        False
-    if(fst(pos) > (size board))
-        False
-    if(snd(pos) > (size board))
-        False
-    else True
+posWithinBounds pos board
+    | fst pos < 0             = False
+    | snd pos < 0             = False
+    | fst pos > size board    = False
+    | snd pos > size board    = False
+    | otherwise               = True
 
--- Checks if position is empty on board
+-- Checks if position is empty on board (does not guarantee cell is within bounds of board)
 cellAtPosIsEmpty :: Position -> Board -> Bool
-cellAtPosIsEmpty pos board= 
-    let bPieces = (pieces board)
-    if [True, x <- bPieces, fst x == pos]
-        False
-    else True
+cellAtPosIsEmpty pos board
+    | isNothing piece      = True
+    | otherwise            = False
+    where piece = pieceAtPos pos board
+
+-- Gets piece at position, if there isn't a piece in that position, returns Nothing
+pieceAtPos :: Position -> Board -> Maybe (Position, Col)
+pieceAtPos pos board
+        | null matchingPos      = Nothing
+        | otherwise             = Just (head matchingPos)
+        where matchingPos = [x | x <- (pieces board), fst x == pos]
 
 -- Check whether the board is in a winning state for either player.
 -- Returns 'Nothing' if neither player has won yet
 -- Returns 'Just c' if the player 'c' has won
+-- Proposed slight change: Assume this function is called after every move has been completed and only
+-- at the position of the last added piece
 checkWon :: Board -> Maybe Col
-checkWon = undefined
+checkWon board
+    | checkWonDirection latestPiece board   = Just (snd latestPiece)
+    | otherwise                             = Nothing
+    where latestPiece = head (pieces board)
 
-{- Hint: One way to implement 'checkWon' would be to write functions 
-which specifically check for lines in all 8 possible directions
-(NW, N, NE, E, W, SE, SW)
+-- Runs the checking functions in all directions looking for a winning condition for the piece provided.
+-- Could (should) be optimized to not have to run all directions - just one direction returning true is enough.
+checkWonAllDirections :: (Position, Col) -> Board -> Bool
+checkWonAllDirections piece board
+    | True `elem` xs        = True
+    | otherwise             = False
+    where xs = [checkWonDirection x piece board | x <- xss]
+          xss = [NorthEast, North, NorthWest, West, SouthWest, South, SouthEast, East]
 
-In these functions:
-To check for a line of n in a row in a direction D:
-For every position ((x, y), col) in the 'pieces' list:
-- if n == 1, the colour 'col' has won
-- if n > 1, move one step in direction D, and check for a line of
-  n-1 in a row.
--}
+-- Checks if the correct amount of pieces in a row in a direction are present.
+-- Returns true if that is the case
+checkWonDirection :: Direction -> (Position, Col) -> Board -> Bool
+checkWonDirection dir piece board
+    | checkConsecutiveInDirection dir (Just piece) board == (target board) = True
+    | otherwise                                                            = False
+
+-- Checks how many pieces in a row in a direction.
+-- The color of the piece is derived from the given piece.
+-- Adds one at the end because there will always be at least one in a row.
+-- (Because of the initial piece being the player's own)
+checkConsecutiveInDirection :: Direction -> Maybe (Position, Col) -> Board -> Int
+checkConsecutiveInDirection dir piece board
+    | isNothing nextpiece                                   = 1
+    | isSameColor (fromJust piece) (fromJust nextpiece)     = 1 + checkConsecutiveInDirection dir nextpiece board
+    | otherwise                                             = 1
+    where nextpiece = getPieceInDirection dir (fromJust piece) board
+
+-- Function to return whatever is in the position in a direction from the given piece.
+-- Returns nothing if the position is empty or out of bounds.
+getPieceInDirection :: Direction -> (Position, Col) -> Board -> Maybe (Position, Col)
+getPieceInDirection dir piece board 
+    | isNothing newpiece      = Nothing
+    | otherwise               = newpiece
+    where position = getPosInDirection dir (fst piece)
+          newpiece = pieceAtPos position board 
+
+-- Takes a position and returns a position in the direction provided
+getPosInDirection :: Direction -> Position -> Position
+getPosInDirection NorthEast (x,y) = (x + 1, y + 1)
+getPosInDirection North (x,y)     = (x, y + 1)
+getPosInDirection NorthWest (x,y) = (x - 1, y + 1)
+getPosInDirection West (x,y)      = (x - 1, y)
+getPosInDirection SouthWest (x,y) = (x - 1, y - 1)
+getPosInDirection South (x,y)     = (x, y - 1)
+getPosInDirection SouthEast (x,y) = (x + 1, y - 1)
+getPosInDirection East (x,y)      = (x + 1, y)
+
+-- Checks if two pieces are of the same color.
+
+isSameColor :: (Position, Col) -> (Position, Col) -> Bool
+isSameColor piece1 piece2
+    | snd piece1 == snd piece2 = True
+    | otherwise                = False
+
+
+-- Helper function to get the color at a position on the board
+getColAtPos :: Position -> Board -> Maybe Col
+getColAtPos pos board
+    | isNothing piece       = Nothing
+    | otherwise             = Just (snd (fromJust piece))
+    where  piece = pieceAtPos pos board
 
 -- An evaluation function for a minimax search. Given a board and a colour
 -- return an integer indicating how good the board is for that colour.
